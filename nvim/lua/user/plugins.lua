7a8beb4b6eebd55add083826217b92bc998c8f09
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2023-09-01 00:49:52 -0700

    Install neovim colorizer

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -89,6 +89,9 @@ return packer.startup(function(use)
   use "kyazdani42/nvim-web-devicons"
   use "kyazdani42/nvim-tree.lua"
 
+  -- Colorizer
+  use "norcalli/nvim-colorizer.lua"
+
   -- Languages
   -- use "findango/vim-mdx" -- MDX syntax highlighting
 
