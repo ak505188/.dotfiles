1c5d5b4633559eaebc151842fc7663d56ef211e6
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2025-12-19 04:09:20 -0800

    Remove unnecessary require

--- nvim/lua/user/lsp/init.lua
+++ nvim/lua/user/lsp/init.lua
@@ -1,15 +1,3 @@
-local mason_ok, mason = pcall(require, "mason")
-if not mason_ok then
-  print "mason not okay"
-	return
-end
-
-local mason_lspconfig_ok, mason_lspconfig = pcall(require, "mason-lspconfig")
-if not mason_lspconfig_ok then
-  print "mason_lspconfig not okay"
-	return
-end
-
 vim.lsp.config['shopify_theme_ls'] = {
   -- Filetypes to automatically attach to.
   filetypes = { 'liquid' },
@@ -31,6 +19,4 @@ vim.lsp.config('lua_ls', {
   }
 })
 
-mason.setup()
-mason_lspconfig.setup()
 require('user.lsp.handlers').setup()
