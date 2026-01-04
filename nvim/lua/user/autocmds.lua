8eb03aced1c582e43ef3a51fd200c69fc4748289
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2026-01-04 03:01:34 -0800

    Autocmd to start treesitter in jsx files

--- nvim/lua/user/autocmds.lua
+++ nvim/lua/user/autocmds.lua
@@ -17,3 +17,16 @@ vim.api.nvim_command([[
 vim.cmd([[
   autocmd BufNewFile,BufReadPost *.mdx set filetype=markdown.mdx
 ]])
+
+-- Set .jsx to jsx filetype
+-- vim.cmd([[
+--   autocmd BufNewFile,BufReadPost *.jsx set filetype=jsx
+-- ]])
+-- treesitter doesnt want to start on jsx files because filetype is read a javascriptreact
+-- This manually starts on jsx files
+vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufReadPost' }, {
+  pattern = { '*.jsx' },
+  callback = function()
+    vim.treesitter.start()
+  end
+})
