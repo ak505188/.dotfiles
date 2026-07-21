ace6516ee7c029a783daf511a99ea310d8f9d2a6
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2026-07-21 06:53:25 -0500

    Fix = fixing indentation

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -30,7 +30,7 @@ return {
         vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
         vim.wo[0][0].foldmethod = 'expr'
         -- Indentation
-        vim.bo.indentexpr = 'v:lua.require"nvim-treesitter".indentexpr()'
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
       end,
     })
   end,
